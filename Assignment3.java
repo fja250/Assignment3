93a514bbb2616d94f4190ae0a4f7bd7cef70b066
Fernando Ayala <fja250@email.vccs.edu>
2018-01-28 20:15:39 -0500

    first commit

---
+++ Assignment3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Assignment3 {
+	public static void main(String[] args) {
+
+	Scanner input = new Scanner(System.in);;
+	System.out.print("Enter a point with two coordinates: ");
+
+	Double x1 = input.nextDouble();
+	Double y1 = input.nextDouble();
+
+	if ( x1 > 5 || y1 > 2.5 )
+		System.out.println("Point (" + x1 + " , " + y1 + ") is not in the rectangle");
+	else
+
+		System.out.println("Point (" + x1 + " , " + y1 + ") is in the rectangle");
+	
+	}
+}
